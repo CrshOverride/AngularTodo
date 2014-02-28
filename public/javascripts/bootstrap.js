8387b1bf234130a57a911a1c78ebd6c007b81db3
Justin Niessner <CrshOverride@gmail.com>
2014-02-28 11:49:30 -0500

    Change Angular app.'s to angularApp.js to make Azure happy.

--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -1,4 +1,4 @@
-define(['require','angular','app','routes'], function(require, angular, app, routes) {
+define(['require','angular','angularApp','routes'], function(require, angular, app, routes) {
 	require(['domReady!'], function(document) {
 		angular.bootstrap(document.getElementById('todo-app-container'), ['todoApp']);
 	});
